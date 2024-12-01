510ae50b6aa05dd220a15bbc52ea490fcb759899
Mattias Hansson <hansson.mattias@gmail.com>
2024-12-01 11:56:06 +0100

    day-1: Part 2

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
     path::Path,
@@ -21,6 +22,28 @@ fn distance(left: &[u32], right: &[u32]) -> u32 {
         .sum();
 
     combined
+}
+
+fn similarity_score(left: &[u32], right: &[u32]) -> u32 {
+    let mut map = HashMap::new();
+
+    right.iter().for_each(|x| match map.get(x) {
+        Some(v) => {
+            map.insert(x, *v + 1);
+        }
+        None => {
+            map.insert(x, 1);
+        }
+    });
+
+    left.iter().fold(0, |mut acc, x| {
+        if let Some(right_score) = map.get(x) {
+            acc += x * right_score;
+            acc
+        } else {
+            acc
+        }
+    })
 }
 
 fn file_to_lists(file: &Path) -> Result<(Vec<u32>, Vec<u32>)> {
@@ -78,12 +101,15 @@ fn main() -> Result<()> {
     let result = distance(&left, &right);
     println!("distance: {result}");
 
+    let result = similarity_score(&left, &right);
+    println!("similarity score: {result}");
+
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::distance;
+    use crate::{distance, similarity_score};
 
     #[test]
     fn test_distance() {
@@ -96,5 +122,18 @@ mod tests {
         let r = distance(&left, &right);
 
         assert_eq!(11, r);
+    }
+
+    #[test]
+    fn test_similarity_score() {
+        let mut left: Vec<u32> = vec![3, 4, 2, 1, 3, 3];
+        let mut right: Vec<u32> = vec![4, 3, 5, 3, 9, 3];
+
+        left.sort();
+        right.sort();
+
+        let r = similarity_score(&left, &right);
+
+        assert_eq!(31, r);
     }
 }
